d405e6b907c7b981198b1f2414533e3fc1e4b02b
snekaC2024 <sneka202420@gmail.com>
2024-08-09 17:34:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(value) {
+    var display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    var display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    var display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
